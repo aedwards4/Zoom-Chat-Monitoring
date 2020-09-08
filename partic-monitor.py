660f2e35031ba9c39231d62088ffb7aff17969a0
Alexis <aedwards4@hawk.iit.edu>
2020-09-07 21:27:59 -0500

    Completed the readChat() method as well and also completed testing on two sample files. Current output is for testing purposes later, and will be removed upon completion of project.

--- partic-monitor.py
+++ partic-monitor.py
@@ -1,48 +1,74 @@
 #!/usr/bin/env python3
 import sys
 
-importFile = sys.argv[1] = #import file
-masterFile = sys.argv[2] = #master database file
+#assigning terminal arguments
+importFile = sys.argv[1] #import file
+masterFile = sys.argv[2] #master database file
 
 #name and input dict
 chatContr = {}
 
 #reject inputs containing a greeting
-greetings = ['good morning', 'hi', 'hello', 'hey', 'thank you', 'bye']
+greetings = ['good morning', "morning", 'hi', 'hello', 'hey', 'thank you', 'bye', "have a good", "have a nice"]
 
 #reject inputs == an interjection
 interjections = ['oh', 'lol']
 
-def readChat(importFile)
-#import chat log
-    inputFile = open(importFile, 'r'):
-        #for every entry
-        for line in inputFile.readlines():
-            #parse name and input, returns list of strings
-            #maxSplit will be determined with test data
-            line.split(' ', maxSplit)     #Change this based on sample data
-                #name = line[#nameIndex]
-                #input = line[#messageIndex]
-            #if name does not already exist, add to dict
-            if name not in chatContr:
-                chatContr[name] = 0
-            #check validity of input (to optimize, skip keys with value of 1)
-            #if input valid,
-                #if value != 1, set it to one
+def readChat(importFile):
+    #import chat log
+    inputFile = open(importFile, 'r')
 
-#Might not be necessary?:
-#iterate over dictionary
-    #if value == 1:
-        #participation point = 1
-    #else:
-        #participation point = 0
+    #collecting data from chat log
+    for line in inputFile.readlines():
+        #parse name and input, returns list of strings
+        spl1 = line.split(' ', 1)
+            #spl1[0] = time
+            #spl2[1] = "From (name) : (message)"
+        spl2 = spl1[1].split(':', 1)
+            #spl2[0] = "From (name)"
+            #spl2[1] = message
+        text = spl2[1]                          #All messages end in '\n'
+        spl3 = spl2[0].split(' ', 1)
+            #spl3[0] = "From"
+            #spl3[1] = name
+        name = spl3[1]
 
-#write dictionary keys and values to a temporary file
+        #if student already participated at least once, no need to check again
+        if name in chatContr and chatContr[name] >= 1:
+            continue
 
-#run bash script to combine the temporary file and the masterFile?
+        #if name does not already exist, add to dict
+        if name not in chatContr:
+            chatContr[name] = 0
+
+        #check validity of input (to optimize, skip keys with value of 1)
+        temp = [x for x in greetings if(x in text)]
+        #if input valid,
+        if bool(temp) == False:
+            chatContr[name] = chatContr[name] + 1
+
+    print(*chatContr.items(), sep='\n')                        #TESTING
+
+readChat(importFile)
+
+#When adding to master list:
+#- Increment total number of classes
+#- Check if name is already in file
+#- If name already exists, increment the count
+#- Else (name does not exist), add the name and assign a value of 1
+#- Final count at end of semester would be # of classes worthy of participation points
+#- Student count/total number of classes = participation grade
+
+#Potential algorithm:
+#   1) write dictionary keys and values to a temporary file
+#   2) run bash script to combine the temporary file and the masterFile?
 
 
-#TO ADD:
-#starting iteration after certain time
-
-main()
+#POTENTIAL CHANGES/ADDITIONS:
+# 1) User input of how participation will be tracked
+#       - 't' (2 args) = only records input between specified time stamps
+#                        (actually 4 args with files)
+#       - 'r' (3 args) = chat will be monitored for inappropriate language
+#                       specified in given .txt file, reading from given
+#                       chat log and outputting names and messages to
+#                       provided file
