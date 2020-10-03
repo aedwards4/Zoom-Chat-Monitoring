97234af7598060604b6fce4adc09016225439f2c
Alexis <aedwards4@hawk.iit.edu>
2020-10-02 21:29:12 -0500

    Updating test files

--- partic-monitor.py
+++ partic-monitor.py
@@ -1,29 +1,42 @@
 #!/usr/bin/env python3
+from timeMonitor import *
 import sys
 
 #assigning terminal arguments
 importFile = sys.argv[1] #import file
 masterFile = sys.argv[2] #master database file
+flag = 'None'   ##FIX FLAG ASSGN LATER
 
-#name and input dict
-chatContr = {}
+if flag == '-t':
+    pass
+elif flag == '-r':
+    pass
+elif flag == 'i':
+    pass
 
-#reject inputs containing a greeting
-greetings = ['good morning', "morning", 'hi', 'hello', 'hey', 'thank you', 'bye', "have a good", "have a nice"]
+#START INPUT MONITOR FUNCTION
 
-#reject inputs == an interjection
-interjections = ['oh', 'lol']
+def allParticipation(importFile):
+    #name and input dict
+    chatContr = {}
 
-def readChat(importFile):
+    #reject inputs containing a greeting
+    greetings = ['good morning', "morning", 'hi', 'hello', 'hey', 'thank you', 'bye', "have a good", "have a nice"]
+
+    #reject inputs == an interjection
+    interjections = ['oh', 'lol']
+
     #import chat log
     inputFile = open(importFile, 'r')
 
     #collecting data from chat log
     for line in inputFile.readlines():
+        if line == '\n':
+            continue                            #NEED TO TEST
         #parse name and input, returns list of strings
         spl1 = line.split(' ', 1)
             #spl1[0] = time
-            #spl2[1] = "From (name) : (message)"
+            #spl1[1] = "From (name) : (message)"
         spl2 = spl1[1].split(':', 1)
             #spl2[0] = "From (name)"
             #spl2[1] = message
@@ -49,7 +62,53 @@ def readChat(importFile):
 
     print(*chatContr.items(), sep='\n')                        #TESTING
 
-readChat(importFile)
+
+def timeMonitor(importFile, start, end):
+    #name and input dict
+    chatContr = {}
+
+    #import chat log
+    inputFile = open(importFile, 'r')
+
+    #collecting data from chat log
+    for line in inputFile.readlines():
+        if line == '\n':
+            continue                            #NEED TO TEST
+        #parse name and input, returns list of strings
+        spl1 = line.split(' ', 1)
+            #spl1[0] = time
+            #spl1[1] = "From (name) : (message)"
+
+        #skip out of bounds times
+        #if spl[0] >                #START HERE
+
+        spl2 = spl1[1].split(':', 1)
+            #spl2[0] = "From (name)"
+            #spl2[1] = message
+        text = spl2[1]                          #All messages end in '\n'
+        spl3 = spl2[0].split(' ', 1)
+            #spl3[0] = "From"
+            #spl3[1] = name
+        name = spl3[1]
+
+        #if student already participated at least once, no need to check again
+        if name in chatContr and chatContr[name] >= 1:
+            continue
+
+        #if name does not already exist, add to dict
+        if name not in chatContr:
+            chatContr[name] = 0
+
+        #check validity of input (to optimize, skip keys with value of 1)
+        temp = [x for x in greetings if(x in text)]
+        #if input valid,
+        if bool(temp) == False:
+            chatContr[name] = chatContr[name] + 1
+
+    print(*chatContr.items(), sep='\n')                        #TESTING
+
+#allParticipation(importFile)
+timeMonitor(importFile, 0, 0)
 
 #When adding to master list:
 #- Increment total number of classes
@@ -72,3 +131,4 @@ readChat(importFile)
 #                       specified in given .txt file, reading from given
 #                       chat log and outputting names and messages to
 #                       provided file
+#       - 'i' (2 args) = inspect the messages of a specific participant
